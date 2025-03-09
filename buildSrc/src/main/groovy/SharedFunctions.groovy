2951ad21c08abcdc4d43173e3bc5f323c539339c
应卓 <yingzhor@gmail.com>
2025-03-09 09:38:50 +0800

    2025-03-09 09:38:50

---
+++ buildSrc/src/main/groovy/SharedFunctions.groovy
@@ -0,0 +1,69 @@
+/*
+ *
+ * Copyright 2022-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+import org.gradle.api.GradleException
+import org.gradle.api.Project
+
+import java.time.LocalDateTime
+
+/**
+ * 构建逻辑的共享函数
+ */
+class SharedFunctions {
+
+	private SharedFunctions() {
+		super();
+	}
+
+	static String getTimestamp(String formatPattern = 'yyyyMMddHHmmss') {
+		return LocalDateTime.now().format(formatPattern)
+	}
+
+	static List<String> getLeafProjectNames(Project rootProject) {
+		return rootProject.allprojects
+			.findAll {
+				it.subprojects.isEmpty()
+			}
+			.collect {
+				it.displayName
+					.replace("project '", "")
+					.replace("'", "")
+			}
+	}
+
+	static String getGradleProperty(Project project, String propertyName, String defaultPropertyValue = null) {
+		var value = project.providers.gradleProperty(propertyName).getOrElse(defaultPropertyValue)
+		if (value == null) {
+			throw new GradleException("Cannot get value of name: ${propertyName}")
+		}
+		return value
+	}
+
+	static boolean getGradlePropertyAsBoolean(Project project, String propertyName, boolean defaultValue = false) {
+		var booleanString = getGradleProperty(project, propertyName, "${defaultValue}")
+		return Boolean.valueOf(booleanString)
+	}
+
+	static String getEnv(String name, String defaultValueIfMissing = '<no value>') {
+		return Optional.of(System.getenv(name)).orElse(defaultValueIfMissing)
+	}
+
+	static boolean getEnvAsBoolean(String name, boolean defaultValue = false) {
+		return getEnv(name, "${defaultValue}").toBoolean()
+	}
+
+}
